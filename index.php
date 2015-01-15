263f572054e92530d0223a4735e0e11c81949882
Kristian Kaa <kristiankaa@lkwebudvikling.dk>
2015-01-15 13:44:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <title>Vesterlauget Madbodega</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      body {
+        font-family: 'Times New Roman';
+        text-align: center;
+        margin: 100px;
+        line-height: 30px;
+        font-size: 20px;
+      }
+      #content {
+        width: 729px;
+        margin: 0 auto;
+      }
+      #facebook {
+        text-align: right;
+      }
+      #facebook img {
+        width: 40px;
+        height: auto;
+        margin-right: 32px;
+      }
+      p {
+        //text-transform: Uppercase;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="content">
+      <img src="logo.png">
+      <p>VESTERGADE 50 - 8000 AARHUS C - TLF. 86 18 21 00</p>
+      <!-- <p>Vi tager imod bordreservationer på min. 8 personer</p> -->
+      <p>MAIL. KONTAKT@VESTERLAUGET.DK</p>
+      <div id="facebook">
+        <a href="http://www.facebook.com/pages/Vesterlauget/417341428333162?fref=ts">
+          <img src="facebook.png" title="Besøg os på Facebook her" />
+        </a>
+      </div>
+    </div>
+  </body>
+</html>
