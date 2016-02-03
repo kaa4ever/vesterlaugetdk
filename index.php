4eaf5cc99f4e3dde092e154d90673c1bab5f1a6f
Kristian Kaa <kristian.kaa@obrigado.dk>
2016-02-03 10:40:47 +0100

    Added favicon

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
         //text-transform: Uppercase;
       }
     </style>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body>
     <div id="content">
